424ea01d3425962b73d6a206859b79be933d3d91
AR Drawing Dev <dev@ar-drawing.local>
2026-08-17 18:53:11 -0300

    Add web camera integration with getUserMedia API

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2524790137" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "380371913" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
